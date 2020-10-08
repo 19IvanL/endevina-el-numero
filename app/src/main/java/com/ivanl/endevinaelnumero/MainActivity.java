4118a44ccc71480a98a7cf269a3bb197d7f13443
Iván <19lopezivan@gmail.com>
2020-10-08 19:09:55 +0200

    Added high score submitting

--- app/src/main/java/com/ivanl/endevinaelnumero/MainActivity.java
+++ app/src/main/java/com/ivanl/endevinaelnumero/MainActivity.java
@@ -2,6 +2,7 @@ package com.ivanl.endevinaelnumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,37 +12,38 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int tries = 0, wins = 0, loses = 0;
+    int tries = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // We generate a random number between 1 and 100 (both included)
+        final int randomNumber = (int)(Math.random() * 100 + 1);
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 EditText text = findViewById(R.id.editTextNumberSigned);
                 String valueString = text.getText().toString();
-                // Program checks whether the EditText is empty or not
+                // First, the program checks whether the EditText is empty or not
                 if (!valueString.matches("")) {
                     // The number introduced by the user is returned
                     int value = Integer.parseInt(text.getText().toString());
                     // Program checks if the value is between the allowed rank
                     if (!(value < 0 || value > 100)) {
-                        // We generate a random number between 1 and 100 (both included)
-                        int randomNumber = (int) (Math.random() * 100 + 1);
-                        String result = Integer.toString(randomNumber);
-                        // We check the result and notify the user about it
-                        String notify;
+                        // The result is checked and the user is notified about it
+                        String notify = "";
                         if (value == randomNumber) {
-                            wins++;
-                            notify = "¡Bien hecho!";
+                            Intent intent = new Intent(v.getContext(), RankingActivity.class);
+                            startActivity(intent);
                         } else {
-                            loses++;
-                            notify = "Número no igual: " + result;
+                            if (randomNumber > value)
+                                notify = "El número es más grande";
+                            else
+                                notify = "El número es más pequeño";
+                            Toast toast = Toast.makeText(MainActivity.this, notify, Toast.LENGTH_SHORT);
+                            toast.show();
                         }
-                        Toast toast = Toast.makeText(MainActivity.this, notify, Toast.LENGTH_SHORT);
-                        toast.show();
                         // One try is added
                         tries++;
                         final TextView textView = findViewById(R.id.textView3);
@@ -50,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
                         Toast toast = Toast.makeText(MainActivity.this, "El número debe estar entre 1 y 100", Toast.LENGTH_SHORT);
                         toast.show();
                     }
-                    // We clear the EditText
+                    // EditText is cleared
                     text.getText().clear();
                 } else {
                     Toast toast = Toast.makeText(MainActivity.this, "Campo vacío", Toast.LENGTH_SHORT);
