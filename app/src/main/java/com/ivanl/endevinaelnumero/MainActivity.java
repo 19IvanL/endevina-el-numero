08b9690ed668444927711a76a790776053b7a8a0
Iván <19lopezivan@gmail.com>
2020-10-22 19:01:19 +0200

    [NON-STABLE] Use Dialog instead of Activity for the Game Over

--- app/src/main/java/com/ivanl/endevinaelnumero/MainActivity.java
+++ app/src/main/java/com/ivanl/endevinaelnumero/MainActivity.java
@@ -8,9 +8,13 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.InsetDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -94,6 +98,10 @@ public class MainActivity extends AppCompatActivity {
         builder.setTitle("Fin del juego");
         builder.setMessage("Introduce tu nombre de usuario para guardar tu puntuación:");
         builder.setView(R.layout.dialog_gameover);
+        TextView tvTries = findViewById(R.id.tvTries);
+        tvTries.setText("Hola");
+        TextView tvTime = findViewById(R.id.tvTime);
+        tvTime.setText("Adios");
         builder.setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int id) {
